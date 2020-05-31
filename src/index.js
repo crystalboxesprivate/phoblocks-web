97b94cd355eebdba4e67bfc49cedaaf93a7d3b5c
crystalboxes <ursus95@gmail.com>
2020-05-31 13:21:42 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import frag from './frag.glsl'
+
+ReactDOM.render(<div>{frag}</div>, document.getElementById('main'))
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.glsl$/,
+        loader: 'webpack-glsl-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+}
